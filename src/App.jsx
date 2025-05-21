d920154e8f04f3696b3fdd663bb3ffbf84ba86e6
abnafis17 <bakhtiyarnafis17@gmail.com>
2025-05-21 06:57:37 +0600

    api fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,84 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Search from './components/Search'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Search from "./components/Search";
 
-const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const API_OPTIONS = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${API_KEY}`
-  }
-}
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 function App() {
-  const [search, setSearch] = useState('');
-  const [errorMessage, setErrorMessage] = useState('')
+  const [search, setSearch] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
     try {
-
-    } catch (error){
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+      const data = await response.json();
+      if (data.response === "False") {
+        setErrorMessage(data.Error || "Failed to fetch movies");
+        setMovieList([]);
+        return;
+      }
+      setMovieList(data.results || []);
+    } catch (error) {
       console.error(`Error fetching movies: ${error}`);
-      setErrorMessage()
+      setErrorMessage("Error fetching movies. Please try again later");
+    } finally {
+      setIsLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
+    fetchMovies();
+  }, []);
 
-  },[])
+  console.log(movieList);
 
   return (
     <main>
-      <div className='pattern'/>
+      <div className="pattern" />
       <div className="wrapper">
         <header>
-          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
-          <Search searchTerm = {search} setSearchTerm = {setSearch}/>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+          <Search searchTerm={search} setSearchTerm={setSearch} />
         </header>
 
-        <section className='all-movies'>
+        <section className="all-movies">
           <h2>All Movie</h2>
+
+          {isLoading ? (
+            <p className="text-white">Loading.....</p>
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => {
+                console.log(movie.title);
+                <p className="text-white">{movie.title}</p>
+              })}
+            </ul>
+          )}
         </section>
-        
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
