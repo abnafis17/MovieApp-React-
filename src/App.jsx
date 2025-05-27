9c7b701cda7ba48eb9107e4b6c40abd6b82969f3
abnafis17 <bakhtiyarnafis17@gmail.com>
2025-05-27 06:58:55 +0600

    Movie Poster

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Search from "./components/Search";
+import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -46,7 +48,7 @@ function App() {
     fetchMovies();
   }, []);
 
-  console.log(movieList);
+  console.log("MovieList", movieList);
 
   return (
     <main>
@@ -61,18 +63,17 @@ function App() {
         </header>
 
         <section className="all-movies">
-          <h2>All Movie</h2>
+          <h2 className="mt-[40px]">All Movie</h2>
 
           {isLoading ? (
-            <p className="text-white">Loading.....</p>
+            <p className="text-white"><Spinner /></p>
           ) : errorMessage ? (
             <p className="text-red-500">{errorMessage}</p>
           ) : (
             <ul>
-              {movieList.map((movie) => {
-                console.log(movie.title);
-                <p className="text-white">{movie.title}</p>
-              })}
+              {movieList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie}/>
+              ))};
             </ul>
           )}
         </section>
